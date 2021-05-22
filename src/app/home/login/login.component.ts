f2cfad4dc790b618f887d0beb2fe851ee3a0ad5e
jbwinchesterjw <jbwincheaster@gmail.com>
2021-05-22 12:24:57 -0300

    #JB-001 - iniciando projeto

---
+++ src/app/home/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  usuario = '';
+  senha = '';
+  constructor(private authService: AutenticacaoService) { }
+
+  ngOnInit(): void {
+  }
+  login() {
+    this.authService.autenticar(this.usuario, this.senha).subscribe(() => {
+      console.log('Atenticado com sucesso');
+    }, (error) => {
+      alert('Usuario ou senha invalido !');
+      console.log(error);
+    }
+    );
+  }
+}
