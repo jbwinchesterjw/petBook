5d8e6745ff14e2044045f98e5f70e763b129e100
jbwinchesterjw <jbwincheaster@gmail.com>
2021-06-09 21:51:55 -0300

    Implementação dos serviços de autenticaçãoGuard de login

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,7 +1,9 @@
+import { switchMap } from 'rxjs/operators';
 import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
 import { Animais } from './../animais';
 import { Component, OnInit } from '@angular/core';
 import { AnimaisService } from '../animais.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-lista-animais',
@@ -9,16 +11,25 @@ import { AnimaisService } from '../animais.service';
   styleUrls: ['./lista-animais.component.css']
 })
 export class ListaAnimaisComponent implements OnInit {
-  animais!: Animais;
+  animais$ !: Observable <Animais>; //o $ indica que que será um observable porem não e obrigatorio
   constructor(private usuarioService: UsuarioService, private animaisService : AnimaisService) { }
 
   ngOnInit(): void {
-    this.usuarioService.retornaUsuario().subscribe((usuario) => {
-      const userName = usuario.name ?? '';
-      this.animaisService.listaDoUsuario(userName).subscribe((animais) =>{
-        this.animais = animais;
+
+    this.animais$ = this.usuarioService.retornaUsuario().pipe(
+      switchMap((usuario) => {
+        const userName = usuario.name ?? '';
+        return this.animaisService.listaDoUsuario(userName);
       })
-    })
+    )
+
+    //essa forma não seria um boa pratica
+    // this.usuarioService.retornaUsuario().subscribe((usuario) => {
+    //   const userName = usuario.name ?? '';
+    //   this.animaisService.listaDoUsuario(userName).subscribe((animais) =>{
+    //     this.animais = animais;
+    //   })
+    // })
   }
 
 }
